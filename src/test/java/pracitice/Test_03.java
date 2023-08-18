d6e15fbf82346ccd5c8dd654a2ad2b74ebd39d11
Org-Bhavana-art <bhavanasmitha007@gmail.com>
2023-08-18 16:50:40 +0530

    Emp123 added new file

---
+++ src/test/java/pracitice/Test_03.java
@@ -0,0 +1,11 @@
+package pracitice;
+
+public class Test_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Bhavana Smitha";
+		System.out.println(s);		
+	}
+
+}
