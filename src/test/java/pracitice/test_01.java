26ff50a56244060b1cf1c8c4f4a59a6fb899ad89
devi-github <devireddyporeddy.com>
2023-08-18 14:50:10 +0530

    committing message

---
+++ src/test/java/pracitice/test_01.java
@@ -0,0 +1,18 @@
+package pracitice;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+	import io.github.bonigarcia.wdm.WebDriverManager;
+
+	public class test_01 {
+		public static void main(String[] args) throws InterruptedException {
+			// TODO Auto-generated method stub
+	  WebDriverManager.chromedriver().setup();
+	  ChromeDriver x=new ChromeDriver();
+	  x.get("http://www.google.co.in");
+	  Thread.sleep(5000);
+	  x.close();
+		}
+
+	}
+
+
